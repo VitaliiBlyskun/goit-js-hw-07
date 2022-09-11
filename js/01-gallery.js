51d3ec7e5a8693867b025079731efd66e433a8d1
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2022-09-11 16:28:44 +0300

    add homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const createElements = (array) => {
+  return array.reduce((acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}" 
+      alt="${description}"
+    />
+  </a>`
+    );
+  }, "");
+};
+
+const pictures = createElements(galleryItems);
+
+const mainContainer = document.querySelector(".gallery");
+mainContainer.classList.add("gallery__item");
+
+mainContainer.insertAdjacentHTML("beforeend", pictures);
+
+mainContainer.addEventListener("click", onClickPicture);
+
+function onClickPicture(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // бібліотека basicLightbox
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  document.addEventListener("keydown", onClosePicture);
+
+  function onClosePicture(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const createElements = (array) => {
+    return array.reduce((acc, { preview, original, description }) => {
+      return (
+        acc +
+        `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}" 
+        alt="${description}"
+      />
+    </a>`
+      );
+    }, "");
+  };
+
+
+  
+  const pictures = createElements(galleryItems);
+  
+  const mainContainer = document.querySelector(".gallery");
+  
+  mainContainer.insertAdjacentHTML("beforeend", pictures);
+
+
+// бібліотека SimpleLightbox
+
+const lightbox = new SimpleLightbox('.gallery a', {
+sourceAttr: "href",
+captionType: 'attr',
+captionsData: "alt",
+captionDelay: "250",
+captionPosition: 'bottom',
+});
